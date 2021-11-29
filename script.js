bbef7e1ee9b9f92ea0b28ce9fe1718a5989cc18c
Andrew Monis <5andrewmonis7@gmail.com>
2021-11-29 17:25:33 -0500

    Update
    Updated the first look and the @media

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-var home = document.getElementById('home');
-var about = document.getElementById('about');
-var contact = document.getElementById('contact');
 var mybutton = document.getElementById('myBtn');
-var submit = document.getElementById('submit');
-home.addEventListener('click', function(){
-		window.scroll({
-			top: 90,
-			left: 0,
-			behavior: 'smooth'
-		});
-});
-about.addEventListener('click', function(){
-		window.scroll({
-			top: 1100,
-			left: 0,
-			behavior: 'smooth'
-		});
-});
-contact.addEventListener('click', function(){
-		window.scroll({
-			top: 3200,
-			left: 0,
-			behavior: 'smooth'
-		});
-});
+
 window.onscroll = function(){scrollFunction()};
 
 function scrollFunction(){
