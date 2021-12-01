e50e7c4a316b89ccc601f2c79753cce83f1f5a15
Andrew Monis <5andrewmonis7@gmail.com>
2021-11-30 20:29:19 -0500

    sizing
    made the website more responsive to screen size

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+
+const footer = document.querySelector('.end');
+
+function isInViewPort(e){
+	const rect = e.getBoundingClientRect();
+	return(
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+document.addEventListener('scroll',  function() {
+	if (isInViewPort(footer) == true){
+		document.querySelector('#myBtn').style.bottom = '65px';
+	}
+	else{
+		document.querySelector('#myBtn').style.bottom = '20px';
+	}
+});
+
+
+
 var mybutton = document.getElementById('myBtn');
 
 window.onscroll = function(){scrollFunction()};
@@ -7,8 +32,8 @@ function scrollFunction(){
 		mybutton.style.display = 'block';
 	}else{
 		mybutton.style.display = 'none';
-	}
-}
+	};
+};
 function topFunction(){
 	document.documentElement.scroll({
 		top: 0,
